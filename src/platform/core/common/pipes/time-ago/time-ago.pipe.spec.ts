87768b99fc46ed9f02d83651daaff9b6e25e86af
John Standish <voldron@gmail.com>
2017-03-19 16:46:32 -0700

    Update time-ago.pipe.spec.ts

--- src/platform/core/common/pipes/time-ago/time-ago.pipe.spec.ts
+++ src/platform/core/common/pipes/time-ago/time-ago.pipe.spec.ts
@@ -45,4 +45,32 @@ describe('TdTimeAgoPipe', () => {
     // years
     expect(pipe.transform(time - 1000 * 60 * 60 * 24 * 30 * 12 * 4, time)).toEqual('4 years ago');
   });
+
+  it('should return a future time string', () => {
+    let fixedTime : number = Date.parse("2017-01-01T00:00:00");
+    // 1 second
+    expect(pipe.transform(Date.parse("2017-01-01T00:00:01"), fixedTime)).toEqual('in 1 second');
+    // < 1 minute
+    expect(pipe.transform(Date.parse("2017-01-01T00:00:37"), fixedTime)).toEqual('in 37 seconds');
+    // 1 minute
+    expect(pipe.transform(Date.parse("2017-01-01T00:01:00"), fixedTime)).toEqual('in 1 minute');
+    // < 1 hour
+    expect(pipe.transform(Date.parse("2017-01-01T00:06:00"), fixedTime)).toEqual('in 6 minutes');
+    // 1 hours
+    expect(pipe.transform(Date.parse("2017-01-01T01:00:00"), fixedTime)).toEqual('in 1 hour');
+    // < 1 day
+    expect(pipe.transform(Date.parse("2017-01-01T13:00:00"), fixedTime)).toEqual('in 13 hours');
+    // 1 day
+    expect(pipe.transform(Date.parse("2017-01-02T00:00:00"), fixedTime)).toEqual('in 1 day');
+    // < 1 month
+    expect(pipe.transform(Date.parse("2017-01-18T00:00:00"), fixedTime)).toEqual('in 17 days');
+    // 1 month
+    expect(pipe.transform(Date.parse("2017-02-01T00:00:00"), fixedTime)).toEqual('in 1 month');
+    // < 1 year
+    expect(pipe.transform(Date.parse("2017-04-01T00:00:00"), fixedTime)).toEqual('in 3 months');
+    // 1 year
+    expect(pipe.transform(Date.parse("2018-01-01T00:00:00"), fixedTime)).toEqual('in 1 year');
+    // years
+    expect(pipe.transform(Date.parse("2021-01-01T00:00:00"), fixedTime)).toEqual('in 4 years');
+  });
 });
