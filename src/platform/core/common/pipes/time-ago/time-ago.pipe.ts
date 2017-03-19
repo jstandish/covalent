acb3b4345a9749c610de925defe189bd2bf6668e
John Standish <voldron@gmail.com>
2017-03-19 16:45:08 -0700

    Update time-ago.pipe.ts

--- src/platform/core/common/pipes/time-ago/time-ago.pipe.ts
+++ src/platform/core/common/pipes/time-ago/time-ago.pipe.ts
@@ -4,6 +4,38 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'timeAgo',
 })
 export class TdTimeAgoPipe implements PipeTransform {
+  private transformText(diff: number, transformHelper : (amount : number, unit : string ) => string ): string {
+    if (diff < 60) {
+      return transformHelper(diff || 1, diff < 2 ? "second" : "seconds");      
+    }
+    // Minutes
+    diff /= 60;
+    if (diff < 60) {
+      return transformHelper(diff || 1, diff < 2 ? "minute" : "minutes");
+    }
+
+    // Hours
+    diff /= 60;
+    if (diff < 24) {
+      return transformHelper(diff || 1, diff < 2 ? "hour" : "hours");   
+    }
+    // Days
+    diff /= 24;
+    if (diff < 30) {
+      return transformHelper(diff || 1, diff < 2 ? "day" : "days");         
+    }
+    
+    // Months
+    diff /= 30;
+    if (diff < 12) {
+      return transformHelper(diff || 1, diff < 2 ? "month" : "months"); 
+    }
+
+    // Years
+    diff /= 12;    
+    return transformHelper(diff || 1, diff < 2 ? "year" : "years"); 
+  }
+
   transform(time: any, reference: any): string {
     // Convert time to date object if not already
     time = new Date(time);
@@ -18,51 +50,10 @@ export class TdTimeAgoPipe implements PipeTransform {
     // for calculations, or else speed of tests can bork.
     let startTime: number = isNaN(ref.getTime()) ? Date.now() : ref.getTime();
     let diff: number = Math.floor((startTime - time.getTime()) / 1000);
-
-    if (diff < 2) {
-      return '1 second ago';
-    }
-    if (diff < 60) {
-      return Math.floor(diff) + ' seconds ago';
-    }
-    // Minutes
-    diff = diff / 60;
-    if (diff < 2) {
-      return '1 minute ago';
-    }
-    if (diff < 60) {
-      return Math.floor(diff) + ' minutes ago';
-    }
-    // Hours
-    diff = diff / 60;
-    if (diff < 2) {
-      return '1 hour ago';
-    }
-    if (diff < 24) {
-      return Math.floor(diff) + ' hours ago';
-    }
-    // Days
-    diff = diff / 24;
-    if (diff < 2) {
-      return '1 day ago';
-    }
-    if (diff < 30) {
-      return Math.floor(diff) + ' days ago';
-    }
-    // Months
-    diff = diff / 30;
-    if (diff < 2) {
-      return '1 month ago';
-    }
-    if (diff < 12) {
-      return Math.floor(diff) + ' months ago';
-    }
-    // Years
-    diff = diff / 12;
-    if (diff < 2) {
-      return '1 year ago';
-    } else {
-      return Math.floor(diff) + ' years ago';
-    }
+    
+    return this.transformText(Math.abs(diff),
+              diff <= -1 ? 
+                (amount : number, unit : string ) => `in ${Math.floor(amount)} ${unit}` : 
+                (amount : number, unit : string ) => `${Math.floor(amount)} ${unit} ago`);
   }
 }
